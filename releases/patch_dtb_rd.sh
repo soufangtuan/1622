0d4d9cf4d2ef807cf30d9cf02f00363eeb2b4403
soufangtuan <111952181+soufangtuan@users.noreply.github.com>
2023-08-08 22:09:54 +0800

    Update patch_dtb_rd.sh

--- releases/patch_dtb_rd.sh
+++ releases/patch_dtb_rd.sh
@@ -1,2 +1,8 @@
 #!/bin/sh
-cp /exts/dtb-patch/dva1622.dtb /etc/model.dtb
+dtbfile=/etc.defaults/model.dtb
+while [ ! -f "$dtbfile"  ];do
+        sleep 1
+        echo wait
+done
+cp -vf dva1622.dtb /etc.defaults/model.dtb
+cp -vf dva1622.dtb /var/run/model.dtb
