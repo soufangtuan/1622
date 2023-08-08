087b0fa3217d0f2fd3267d99e4fba9da66220511
soufangtuan <111952181+soufangtuan@users.noreply.github.com>
2023-08-08 14:58:58 +0800

    Update install_rd.sh

--- releases/install_rd.sh
+++ releases/install_rd.sh
@@ -1,6 +1,3 @@
 #!/bin/sh
-touch /tmp/dtbmod
-rm /etc.defaults/model.dtb
-rm /var/run/model.dtb
-cp -vf model_dva1622.dtb /etc.defaults/model.dtb
-cp -vf model_dva1622.dtb /var/run/model.dtb
+mv dva1622.dtb /etc.defaults/model.dtb
+mv dva1622.dtb /var/run/model.dtb
